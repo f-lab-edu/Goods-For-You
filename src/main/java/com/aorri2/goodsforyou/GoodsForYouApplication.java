4a0a7b4627c8e25ab004506e7484bd3e9574d563
aorri2 <aorri96@naver.com>
2023-01-04 23:53:55 +0900

    [#5] feat : 프로젝트 생성

---
+++ src/main/java/com/aorri2/goodsforyou/GoodsForYouApplication.java
@@ -0,0 +1,13 @@
+package com.aorri2.goodsforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsForYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aorri2/goodsforyou/GoodsForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aorri2.goodsforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsForYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
