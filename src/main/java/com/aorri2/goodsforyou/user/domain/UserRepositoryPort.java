96cc7a4bf4a629522f58f22acaf9b579ab9b49ff
aorri2 <aorri96@naver.com>
2023-01-26 00:21:42 +0900

    [#15] refactor : NewUserCreator, NewUserFinder 코드 변경
    - NewUserCreator, NewUserFinder가 UserRepositoryPort를 직접 사용함으로 인해, 필요한 정보 이상의 정보를 알고 있어 UserCreatorPort, UserFinderPort로 분리

--- src/main/java/com/aorri2/goodsforyou/user/domain/UserRepositoryPort.java
+++ src/main/java/com/aorri2/goodsforyou/user/domain/UserRepositoryPort.java
@@ -1,11 +1,5 @@
 package com.aorri2.goodsforyou.user.domain;
 
-public interface UserRepositoryPort {
-
-	void save(User user);
-
-	User findByName(String name);
-
-	User findByEmail(String email);
+public interface UserRepositoryPort extends UserCreatorPort, UserFinderPort {
 
 }
