bf8d1bfd9083986d47c77873b7ee1c9d3e45155d
aorri2 <aorri96@naver.com>
2023-01-25 23:31:07 +0900

    [#15] refactor : NewUserCreator, NewUserFinder 코드 변경
    - NewUserCreator, NewUserFinder가 UserRepositoryPort를 직접 사용함으로 인해, 필요한 정보 이상의 정보를 알고 있어 UserCreatorPort, UserFinderPort로 분리

--- src/main/java/com/aorri2/goodsforyou/user/application/NewUserCreator.java
+++ src/main/java/com/aorri2/goodsforyou/user/application/NewUserCreator.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Component;
 
 import com.aorri2.goodsforyou.user.domain.User;
 import com.aorri2.goodsforyou.user.domain.UserCreator;
+import com.aorri2.goodsforyou.user.domain.UserCreatorPort;
 import com.aorri2.goodsforyou.user.domain.UserRepositoryPort;
 
 @Component
 public class NewUserCreator implements UserCreator {
-	private final UserRepositoryPort userRepositoryPort;
+	private final UserCreatorPort userRepositoryPort;
 
 	public NewUserCreator(UserRepositoryPort userRepositoryPort) {
 		this.userRepositoryPort = userRepositoryPort;
