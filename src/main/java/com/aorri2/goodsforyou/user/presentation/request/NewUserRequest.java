6189d2924d78d8fa7e2d50286dd426803830b146
aorri2 <aorri96@naver.com>
2023-02-02 19:21:16 +0900

    [#21] feat : 이메일 검사를 위한 Custom Validation 추가

--- src/main/java/com/aorri2/goodsforyou/user/presentation/request/NewUserRequest.java
+++ src/main/java/com/aorri2/goodsforyou/user/presentation/request/NewUserRequest.java
@@ -1,5 +1,6 @@
 package com.aorri2.goodsforyou.user.presentation.request;
 
+import com.aorri2.goodsforyou.common.annotation.GfyEmailConstraint;
 import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
 
 import jakarta.validation.constraints.NotBlank;
@@ -7,7 +8,12 @@ import jakarta.validation.constraints.Size;
 
 public class NewUserRequest {
 
-	@NotBlank(message = "이메일은 필수 값 입니다.")
+	/**
+	 * 커스텀 이메일 검증 애노테이션을 통해 @Email에서는 null값을 허용하지만,
+	 * @GfyEmailConstraint 에너테이션을 통해 Null값, 빈 문자열, 공백 문자열을 모두 검증했습니다.
+	 * (@NotBlank + @Email)
+	 */
+	@GfyEmailConstraint(message = "이메일은 필수 값 입니다.")
 	private String email;
 
 	@NotBlank(message = "이름은 필수 값 입니다.")
