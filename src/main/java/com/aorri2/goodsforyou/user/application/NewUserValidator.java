3afbb5fb51e333087f6d88ca724e4a98ac1ba130
aorri2 <aorri96@naver.com>
2023-02-07 23:21:09 +0900

    [#27] refactor : UserPolicy 리스트와 LoginUserPolicy 리스트의 구현방식을 DI 방식으로 변경

--- src/main/java/com/aorri2/goodsforyou/user/application/NewUserValidator.java
+++ src/main/java/com/aorri2/goodsforyou/user/application/NewUserValidator.java
@@ -7,25 +7,18 @@ import org.springframework.stereotype.Component;
 import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
 import com.aorri2.goodsforyou.user.application.command.LoginUserCommand;
 import com.aorri2.goodsforyou.user.domain.LoginUserPolicy;
-import com.aorri2.goodsforyou.user.domain.UserFinder;
 import com.aorri2.goodsforyou.user.domain.UserPolicy;
 import com.aorri2.goodsforyou.user.domain.UserValidator;
-import com.aorri2.goodsforyou.user.domain.policy.LoginUserEmailPolicy;
-import com.aorri2.goodsforyou.user.domain.policy.LoginUserPasswordPolicy;
-import com.aorri2.goodsforyou.user.domain.policy.NewUserEmailPolicy;
-import com.aorri2.goodsforyou.user.domain.policy.NewUserNamePolicy;
-import com.aorri2.goodsforyou.user.domain.policy.NewUserPasswordPolicy;
 
 @Component
 public class NewUserValidator implements UserValidator {
 	private final List<UserPolicy> validityPolicyList;
 	private final List<LoginUserPolicy> loginUserPolicyList;
 
-	public NewUserValidator(UserFinder userFinder) {
-		this.validityPolicyList = List.of(new NewUserEmailPolicy(userFinder), new NewUserNamePolicy(userFinder),
-			new NewUserPasswordPolicy());
-		this.loginUserPolicyList = List.of(new LoginUserEmailPolicy(userFinder),
-			new LoginUserPasswordPolicy(userFinder));
+	public NewUserValidator(List<UserPolicy> validityPolicyList,
+		List<LoginUserPolicy> loginUserPolicyList) {
+		this.validityPolicyList = validityPolicyList;
+		this.loginUserPolicyList = loginUserPolicyList;
 	}
 
 	@Override
