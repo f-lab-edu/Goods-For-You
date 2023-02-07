7ef739856a584e92429e86a8df84b33a05d0ce39
aorri2 <aorri96@naver.com>
2023-02-07 21:30:53 +0900

    [#27] feat : 로그인 기능에 필요한 메서드 추가

--- src/main/java/com/aorri2/goodsforyou/user/application/NewUserValidator.java
+++ src/main/java/com/aorri2/goodsforyou/user/application/NewUserValidator.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
+import com.aorri2.goodsforyou.user.application.command.LoginUserCommand;
+import com.aorri2.goodsforyou.user.domain.LoginUserPolicy;
 import com.aorri2.goodsforyou.user.domain.UserFinder;
 import com.aorri2.goodsforyou.user.domain.UserPolicy;
 import com.aorri2.goodsforyou.user.domain.UserValidator;
+import com.aorri2.goodsforyou.user.domain.policy.LoginUserEmailPolicy;
+import com.aorri2.goodsforyou.user.domain.policy.LoginUserPasswordPolicy;
 import com.aorri2.goodsforyou.user.domain.policy.NewUserEmailPolicy;
 import com.aorri2.goodsforyou.user.domain.policy.NewUserNamePolicy;
 import com.aorri2.goodsforyou.user.domain.policy.NewUserPasswordPolicy;
@@ -15,14 +19,22 @@ import com.aorri2.goodsforyou.user.domain.policy.NewUserPasswordPolicy;
 @Component
 public class NewUserValidator implements UserValidator {
 	private final List<UserPolicy> validityPolicyList;
+	private final List<LoginUserPolicy> loginUserPolicyList;
 
 	public NewUserValidator(UserFinder userFinder) {
 		this.validityPolicyList = List.of(new NewUserEmailPolicy(userFinder), new NewUserNamePolicy(userFinder),
 			new NewUserPasswordPolicy());
+		this.loginUserPolicyList = List.of(new LoginUserEmailPolicy(userFinder),
+			new LoginUserPasswordPolicy(userFinder));
 	}
 
 	@Override
 	public void checkUserValidity(CreateUserCommand user) {
 		validityPolicyList.forEach(policy -> policy.apply(user.toEntity()));
+	}
+
+	@Override
+	public void checkLoginUserValidity(LoginUserCommand command) {
+		loginUserPolicyList.forEach(policy -> policy.apply(command));
 	}
 }
--- src/main/java/com/aorri2/goodsforyou/user/application/facade/NewUserManagement.java
+++ src/main/java/com/aorri2/goodsforyou/user/application/facade/NewUserManagement.java
@@ -2,9 +2,11 @@ package com.aorri2.goodsforyou.user.application.facade;
 
 import org.springframework.stereotype.Service;
 
-import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
-import com.aorri2.goodsforyou.user.domain.UserCreator;
 import com.aorri2.goodsforyou.user.application.UserManagement;
+import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
+import com.aorri2.goodsforyou.user.application.command.LoginUserCommand;
+import com.aorri2.goodsforyou.user.domain.TokenGenerator;
+import com.aorri2.goodsforyou.user.domain.UserCreator;
 import com.aorri2.goodsforyou.user.domain.UserValidator;
 
 @Service
@@ -12,15 +14,23 @@ public class NewUserManagement implements UserManagement {
 
 	private final UserCreator userCreator;
 	private final UserValidator userValidator;
+	private final TokenGenerator tokenGenerator;
 
-	public NewUserManagement(UserCreator userCreator, UserValidator userValidator) {
+	public NewUserManagement(UserCreator userCreator, UserValidator userValidator, TokenGenerator tokenGenerator) {
 		this.userCreator = userCreator;
 		this.userValidator = userValidator;
+		this.tokenGenerator = tokenGenerator;
 	}
 
 	@Override
 	public void joinUser(CreateUserCommand command) {
 		userValidator.checkUserValidity(command);
 		userCreator.save(command.toEntity());
+	}
+
+	@Override
+	public String loginUser(LoginUserCommand command) {
+		userValidator.checkLoginUserValidity(command);
+		return tokenGenerator.generate();
 	}
 }
