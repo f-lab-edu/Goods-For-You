3e3535161d2487f7fdfd04db7e6c5e86b76a56ed
aorri2 <aorri96@naver.com>
2023-02-07 21:32:28 +0900

    [#27] feat : 로그인시 검증을 해주는 객체 추가
    - Exception에 관한 부분은 로그인 기능 PR의 범위를 넘어선다 생각해 현재는 RuntimeException으로 처리했습니다.

---
+++ src/main/java/com/aorri2/goodsforyou/user/domain/policy/LoginUserEmailPolicy.java
@@ -0,0 +1,28 @@
+package com.aorri2.goodsforyou.user.domain.policy;
+
+import org.springframework.stereotype.Component;
+
+import com.aorri2.goodsforyou.user.application.command.LoginUserCommand;
+import com.aorri2.goodsforyou.user.domain.LoginUserPolicy;
+import com.aorri2.goodsforyou.user.domain.User;
+import com.aorri2.goodsforyou.user.domain.UserFinder;
+
+@Component
+public class LoginUserEmailPolicy implements LoginUserPolicy {
+
+	private final UserFinder userFinder;
+
+	public LoginUserEmailPolicy(UserFinder userFinder) {
+		this.userFinder = userFinder;
+	}
+
+	@Override
+	public void apply(LoginUserCommand loginUserCommand) {
+		User foundByEmail = userFinder.findByEmail(loginUserCommand.getEmail());
+
+		if (foundByEmail == null) {
+			throw new RuntimeException("유효하지 않은 회원 이메일 입니다.");
+		}
+
+	}
+}
