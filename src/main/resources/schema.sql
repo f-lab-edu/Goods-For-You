076872fc6091dd8e034c621615e54fae0818d0b9
aorri2 <aorri96@naver.com>
2023-02-15 00:27:27 +0900

    [#31] feat : schema.sql로 스프링 부트 로딩 시 데이터 초기화

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+drop table if exists USER;
+
+CREATE TABLE USER (id  int unsigned  NOT NULL  AUTO_INCREMENT, name VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL,PRIMARY KEY(id));
