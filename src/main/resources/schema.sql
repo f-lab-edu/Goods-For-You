24266428d66916e42541c11cc906b25553596db4
aorri2 <aorri96@naver.com>
2023-02-15 16:56:15 +0900

    [#31] refactor : 스프링 부트 실행시, 테이블 초기화 되지 않도록 변경
    - applcation.yml 파일의 sql init mode 삭제
    - schema.sql의 `drop table`구문 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,8 @@
-drop table if exists USER;
-
-CREATE TABLE USER (id  int unsigned  NOT NULL  AUTO_INCREMENT, name VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL,PRIMARY KEY(id));
+CREATE TABLE USER
+(
+    id       int unsigned NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(100) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
