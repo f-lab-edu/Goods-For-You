f16053d412ed529331bca0d55f2dc545c88bded8
aorri2 <aorri96@naver.com>
2023-03-01 23:38:50 +0900

    [#28] refactor : UserControllerAdvice를 GlobalControllerAdvice로 이름 변경 및 GlobalException만 처리하도록 변경

--- src/main/java/com/aorri2/goodsforyou/common/advice/GlobalControllerAdvice.java
+++ src/main/java/com/aorri2/goodsforyou/common/advice/GlobalControllerAdvice.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.aorri2.goodsforyou.common.domain.ErrorResult;
-import com.aorri2.goodsforyou.exception.BaseException;
-import com.aorri2.goodsforyou.user.domain.exception.DuplicatedEmailException;
-import com.aorri2.goodsforyou.user.domain.exception.DuplicatedNameException;
+import com.aorri2.goodsforyou.common.exception.GoodsForYouException;
 
 @RestControllerAdvice
-public class UserControllerAdvice {
+public class GlobalControllerAdvice {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<ErrorResult> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
@@ -20,25 +18,13 @@ public class UserControllerAdvice {
 		return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
 	}
 
-	@ExceptionHandler(BaseException.class)
 	/**
 	 * handleBaseException 애플리케이션에서 발생한 에러를 처리하는 메서드
 	 */
-	protected ResponseEntity<ErrorResult> handleBaseException(BaseException e) {
+	@ExceptionHandler(GoodsForYouException.class)
+	protected ResponseEntity<ErrorResult> handleGoodsForYouException(GoodsForYouException e) {
 		ErrorResult result = new ErrorResult("COMMON-001", e.getMessage());
 		return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@ExceptionHandler(DuplicatedEmailException.class)
-	protected ResponseEntity<ErrorResult> handleDuplicatedEmailException(DuplicatedEmailException e) {
-		ErrorResult result = new ErrorResult("USER-002", e.getMessage());
-		return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-	}
-
-	@ExceptionHandler(DuplicatedNameException.class)
-	protected ResponseEntity<ErrorResult> handleDuplicatedNameException(DuplicatedNameException e) {
-		ErrorResult result = new ErrorResult("USER-002", e.getMessage());
-		return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
 	}
 
 }
