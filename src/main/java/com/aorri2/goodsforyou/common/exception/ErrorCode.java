21aa8cca636a59fab4d1519caa3f16f0041301ca
aorri2 <aorri96@naver.com>
2023-03-01 23:48:26 +0900

    [#28] refactor : NotMatched예외 클래스들 추가에 따른 ErrorCode값 추가

--- src/main/java/com/aorri2/goodsforyou/common/exception/ErrorCode.java
+++ src/main/java/com/aorri2/goodsforyou/common/exception/ErrorCode.java
@@ -1,11 +1,13 @@
-package com.aorri2.goodsforyou.exception;
+package com.aorri2.goodsforyou.common.exception;
 
 public enum ErrorCode {
 
 	//User Error Code
 	DUPLICATED_EMAIL("이미 존재하는 이메일 입니다."),
 	DUPLICATED_NAME("이미 존재하는 닉네임 입니다."),
-	INVALID_PASSWORD("패스워드는 8자리 이상 입니다.");
+	INVALID_PASSWORD("패스워드는 8자리 이상 입니다."),
+	NOT_MATCHED_PASSWORD("패스워드가 일치하지 않습니다."),
+	NOT_MATCHED_EMAIL("이메일이 일치하지 않습니다.");
 
 	private final String message;
 
