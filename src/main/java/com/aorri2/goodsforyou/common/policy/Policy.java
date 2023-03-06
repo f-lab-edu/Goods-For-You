bfc92444e4876402f4135920f245aae9fa7dc289
aorri2 <aorri96@naver.com>
2023-03-06 17:30:47 +0900

    [#49] refactor : Policy 인터페이스가 추후 여러 기능 패키지에서 사용될 것을 생각해, Common으로 위치 변경

--- src/main/java/com/aorri2/goodsforyou/common/policy/Policy.java
+++ src/main/java/com/aorri2/goodsforyou/common/policy/Policy.java
@@ -1,4 +1,4 @@
-package com.aorri2.goodsforyou.user.domain;
+package com.aorri2.goodsforyou.common.policy;
 
 public interface Policy<T> {
 	void apply(T t);
--- src/main/java/com/aorri2/goodsforyou/user/domain/NewUserPolicy.java
+++ src/main/java/com/aorri2/goodsforyou/user/domain/NewUserPolicy.java
@@ -1,5 +1,6 @@
 package com.aorri2.goodsforyou.user.domain;
 
+import com.aorri2.goodsforyou.common.policy.Policy;
 import com.aorri2.goodsforyou.user.application.command.CreateUserCommand;
 
 public interface NewUserPolicy extends Policy<CreateUserCommand> {
