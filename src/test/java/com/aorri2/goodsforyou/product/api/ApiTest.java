67dfea1ba991d3cdfe95a1e284502b15dd444dc1
aorri2 <aorri96@naver.com>
2023-03-10 02:23:53 +0900

    [#53] feat : API 통합 테스트 설정을 위한 ApiTest 클래스 추가

---
+++ src/test/java/com/aorri2/goodsforyou/product/api/ApiTest.java
@@ -0,0 +1,19 @@
+package com.aorri2.goodsforyou.product.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+	}
+}
