e8b13bc38bcc16a3d9f388164517bdbf5a9e30c6
aorri2 <aorri96@naver.com>
2023-03-10 02:19:06 +0900

    [#53] feat : 상품 추가기능 구현을 위한 테이블 schema추가 및, sql init 설정 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,43 @@
-CREATE TABLE USER
+CREATE TABLE if not exists USER
 (
-    id       int unsigned NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(100) NOT NULL,
-    email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    id         BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(100)    NOT NULL,
+    email      VARCHAR(255)    NOT NULL UNIQUE,
+    password   VARCHAR(255)    NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE if not exists CATEGORY
+(
+    id   BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100)    NULL,
+    primary key (id)
+);
+
+CREATE TABLE if not exists PRODUCT
+(
+    id          BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    category_id BIGINT unsigned NOT NULL,
+    title       VARCHAR(100)    NOT NULL,
+    price       BIGINT unsigned,
+    created_at  DATETIME,
+    updated_at  DATETIME,
+    primary key (id),
+    foreign key (category_id) references CATEGORY (id)
+
+);
+
+CREATE TABLE if not exists PRODUCT_IMAGE
+(
+    id         BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    product_id BIGINT unsigned NOT NULL,
+    name       VARCHAR(100)    NOT NULL,
+    url        VARCHAR(255)    NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    primary key (id),
+    foreign key (product_id) references PRODUCT (id)
+);
+
