adba48d6814e89249b4c78686458154995930596
aorri2 <aorri96@naver.com>
2023-03-31 15:35:14 +0900

    [#64] refactor : Transaction 필드값 네이밍 camelCase로 변경

--- src/main/java/com/aorri2/goodsforyou/transaction/domain/Transaction.java
+++ src/main/java/com/aorri2/goodsforyou/transaction/domain/Transaction.java
@@ -5,25 +5,29 @@ import java.time.LocalDateTime;
 public class Transaction {
 
 	private Long id;
-	private Long buyer_id;
-	private Long seller_id;
-	private Long product_id;
-	private LocalDateTime transaction_date;
-	private TransactionStatus transaction_status;
-	private int transaction_product_quantity;
-	private LocalDateTime created_at;
-	private LocalDateTime updated_at;
+	private Long buyerId;
+	private Long sellerId;
+	private Long productId;
+	private LocalDateTime transactionDate;
+	private TransactionStatus transactionStatus;
+	private int transactionProductQuantity;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
 	protected Transaction() {
 	}
 
-	public Transaction(Long buyer_id, Long seller_id, Long product_id, LocalDateTime transaction_date,
-		int transaction_product_quantity) {
-		this.buyer_id = buyer_id;
-		this.seller_id = seller_id;
-		this.product_id = product_id;
-		this.transaction_date = transaction_date;
-		this.transaction_status = TransactionStatus.BEFORE_TRANSACTION;
-		this.transaction_product_quantity = transaction_product_quantity;
+	public Transaction(Long buyerId, Long sellerId, Long productId, LocalDateTime transactionDate,
+		int transactionProductQuantity) {
+		this.buyerId = buyerId;
+		this.sellerId = sellerId;
+		this.productId = productId;
+		this.transactionDate = transactionDate;
+		this.transactionStatus = TransactionStatus.BEFORE_TRANSACTION;
+		this.transactionProductQuantity = transactionProductQuantity;
+	}
+
+	public Long getProductId() {
+		return productId;
 	}
 }
