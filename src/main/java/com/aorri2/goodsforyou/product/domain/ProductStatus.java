b6990a332a8e7cd5c317e828003a53f131cbd868
aorri2 <aorri96@naver.com>
2023-04-08 20:31:20 +0900

    [#67] feat : 상품 상태에 `판매 취소`상태 추가
    - `ProductStatus` Enum에 `판매 취소`상태를 추가
    - `changeStatus()`메서드를 통해 상품의 상태를 변경 할수 있도록 구현

--- src/main/java/com/aorri2/goodsforyou/product/domain/ProductStatus.java
+++ src/main/java/com/aorri2/goodsforyou/product/domain/ProductStatus.java
@@ -3,6 +3,7 @@ package com.aorri2.goodsforyou.product.domain;
 public enum ProductStatus {
 	TRADABLE("구매 가능"),
 	SOLD_COMPLETE("판매 완료"),
+	SOLD_CANCELED("판매 취소"),
 	;
 	private final String status;
 
@@ -21,4 +22,34 @@ public enum ProductStatus {
 	public boolean isTradable() {
 		return this.equals(TRADABLE);
 	}
+
+	public boolean isSoldComplete() {
+		return this.equals(SOLD_COMPLETE);
+	}
+
+	public ProductStatus changeStatus(ProductStatus status) {
+		switch (status) {
+			case TRADABLE -> {
+				if (isSoldComplete()) {
+					return SOLD_COMPLETE;
+				} else {
+					return TRADABLE;
+				}
+			}
+
+			case SOLD_CANCELED -> {
+				if (isSoldComplete()) {
+					return SOLD_COMPLETE;
+				} else {
+					return SOLD_CANCELED;
+				}
+			}
+
+			case SOLD_COMPLETE -> {
+				return SOLD_COMPLETE;
+			}
+			default -> throw new IllegalArgumentException("유효하지 않은 status입니다.");
+		}
+	}
+
 }
