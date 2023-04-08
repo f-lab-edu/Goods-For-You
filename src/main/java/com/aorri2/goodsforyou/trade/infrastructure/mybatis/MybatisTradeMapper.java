bfd245f3746e243694f344ea9b361effe9644459
aorri2 <aorri96@naver.com>
2023-04-08 20:40:28 +0900

    [#67] refactor : MybatisTradeMapper의 Sql구문의 일관된 간격을 유지하기 위해 수정

--- src/main/java/com/aorri2/goodsforyou/trade/infrastructure/mybatis/MybatisTradeMapper.java
+++ src/main/java/com/aorri2/goodsforyou/trade/infrastructure/mybatis/MybatisTradeMapper.java
@@ -8,6 +8,6 @@ import com.aorri2.goodsforyou.trade.domain.Trade;
 @Mapper
 public interface MybatisTradeMapper {
 
-	@Insert("insert into TRADE (buyer_id, seller_id, product_id,trade_date,trade_status,trade_product_quantity) VALUES (#{buyerId}, #{sellerId}, #{productId}, #{tradeDate},#{tradeStatus}, #{tradeProductQuantity})")
+	@Insert("insert into TRADE (buyer_id, seller_id, product_id, trade_date, trade_status, trade_product_quantity) VALUES (#{buyerId}, #{sellerId}, #{productId}, #{tradeDate}, #{tradeStatus}, #{tradeProductQuantity})")
 	void save(Trade trade);
 }
