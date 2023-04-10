a1d90ae425071aea086129c84be7e55e8caaeafa
aorri2 <aorri96@naver.com>
2023-04-11 00:45:58 +0900

    [#67] feat : 병목지점이 될만한 부분인 데이터베이스 쿼리를 호출하는 부분에 대해 Aop를 이용해, 로그 작성

---
+++ src/main/java/com/aorri2/goodsforyou/common/annotation/ElapsedTime.java
@@ -0,0 +1,11 @@
+package com.aorri2.goodsforyou.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ElapsedTime {
+}
