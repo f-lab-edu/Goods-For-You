0ec70ed5b5fe8755d261b8d807066139cc3f0f25
aorri2 <aorri96@naver.com>
2023-04-14 00:09:15 +0900

    [#67] feat : 로그 포맷에 statusCode 추가

--- src/main/java/com/aorri2/goodsforyou/common/filter/MDCLoggingFilter.java
+++ src/main/java/com/aorri2/goodsforyou/common/filter/MDCLoggingFilter.java
@@ -8,22 +8,29 @@ import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
 public class MDCLoggingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
 		IOException,
 		ServletException {
+		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(
+			(HttpServletResponse)response);
+		log.info("WrappedResponseStatusInFilter = {}", responseWrapper.getStatus());
 		MDC.put("request_id", randomUUID().toString());
-		chain.doFilter(request, response);
+		chain.doFilter(request, responseWrapper);
 		MDC.clear();
 	}
 }
