a20c22fc873c5bc5eae159a24b53231f89f96c26
aorri2 <76669404+aorri2@users.noreply.github.com>
2023-09-20 20:38:16 +0900

    [#92] 서버 비용 문제로 인해, 상품 이미지 관련 모듈을 삭제합니다

--- src/main/java/com/aorri2/goodsforyou/productimage/domain/exception/UnSupportedFileTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.aorri2.goodsforyou.productimage.domain.exception;
-
-import com.aorri2.goodsforyou.common.exception.ErrorCode;
-
-public class UnSupportedFileTypeException extends ProductImageException {
-	public UnSupportedFileTypeException() {
-		super(ErrorCode.NOT_SUPPORTED_FILE_TYPE);
-	}
-}
